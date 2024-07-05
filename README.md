b5f987b9567f5cd7ec4ba88c5ece13e8fcb05d8e
Joe Vaughan <jbvaughan1993@gmail.com>
2024-07-05 12:39:35 +0100

    Added example gallery

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kungfu Kitchen -- Astro
+# Kungfu Kitchen – Astro site 🚀
+
+This is a WIP version of the site for [Kungfu Kitchen](https://kungfureading.co.uk/) (Reading's best Chinese restaurant, IMHO) remade with Astro.
--- src/components/foodGallery.tsx
+++ src/components/foodGallery.tsx
@@ -1,40 +1,63 @@
 import { useState } from "react";
-import restaurant from "../assets/kfk-restaurant.webp";
+import "../../public/kfk-restaurant.webp";
+
+interface ReactButtonProps {
+  cta: string;
+  func: () => void;
+}
+
+function ReactButton({ cta, func }: ReactButtonProps) {
+  return (
+    <button onClick={func} className="bg-red text-white px-2 rounded shadow-sm">
+      {cta}
+    </button>
+  );
+}
 
 export default function Gallery() {
+  const imageList = [
+    { src: "/kfk-restaurant.webp", id: 0, alt: "Sichuan chicken" },
+  ];
   const [imageIndex, setImageIndex] = useState(0);
-  const imageList = [restaurant, "/image2.jpeg"];
 
-  const handleNextImage = () => {
+  function handleNextImage() {
     if (imageIndex < imageList.length - 1) {
       setImageIndex(imageIndex + 1);
+    } else {
+      setImageIndex(0); // Loop back to the first image
     }
-  };
+    console.log("Image index is: " + imageIndex);
+  }
 
-  const handlePreviousImage = () => {
+  function handlePreviousImage() {
     if (imageIndex > 0) {
       setImageIndex(imageIndex - 1);
+    } else {
+      setImageIndex(imageList.length - 1); // Loop back to the last image
     }
-  };
+    console.log("Image index is: " + imageIndex);
+  }
 
   return (
-    <div className="mb-2 relative">
-      <button className="relative" onClick={handleNextImage}>
-        +
-      </button>
-      <button className="relative" onClick={handlePreviousImage}>
-        -
-      </button>
-      <div className="absolute">
-        {imageList.map((src, index) => (
-          <img
-            key={index}
-            alt={imageIndex.toString()}
-            className={index === imageIndex ? "block" : "hidden"}
-          />
-        ))}
+    <div className="relative mb-2">
+      <div className="relative">
+        {imageList.map(
+          (image) =>
+            image.id === imageIndex && (
+              <img
+                className="w-full h-auto rounded-md"
+                key={image.id}
+                src={image.src}
+                alt={image.alt}
+              />
+            )
+        )}
+        <div className="absolute inset-x-0 bottom-0 flex items-center justify-between p-4 bg-black bg-opacity-75 rounded-md">
+          <ReactButton func={handlePreviousImage} cta="-" />
+          <div className="text-white mx-2">{imageList[imageIndex].alt}</div>
+          <ReactButton func={handleNextImage} cta="+" />
+        </div>
       </div>
-      image is {imageIndex}
     </div>
   );
 }
