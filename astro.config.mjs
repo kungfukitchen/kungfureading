20fb790ad1d5e764c9644bfac3893496f298da33
Joe Vaughan <jbvaughan1993@gmail.com>
2024-09-06 19:23:40 +0100

    New commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), react()]
+  integrations: [tailwind(), icon()]
 });
